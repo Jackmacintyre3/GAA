3752aa64c9041545363a6a2f30432fd3221c9cdc
Jack <L00158710@atu.ie>
2024-06-23 15:06:44 +0100

    Merge remote main into local main

--- public/nav.js
+++ public/nav.js
@@ -1,12 +1,14 @@
-document.write('<div>\ <img src = logos/logo.png height = 60px  \
-<a href = "http://localhost:3000/routes.html">Routes</a> | \
-<a href = "http://localhost:3000/teams.html">Teams</a> | \
-<a href = "http://localhost:3000/players.html">Players</a> | \
-<a href = "http://localhost:3000/today.html">Today</a> | \
-<a href = "http://localhost:3000/fixtures.html">Fixtures</a> | \
-<a href = "http://localhost:3000/results.html">Results</a> | \
-<a href = "http://localhost:3000/standings.html">Standings</a> | \
-<a href = "http://localhost:3000/login.html">Login</a>  \
-<hr>\
-</div>\
-');
+document.write(`
+<div>
+  <img src="logos/logo.png" height="60px" />
+  <a href="routes.html">Routes</a> |
+  <a href="teams.html">Teams</a> |
+  <a href="players.html">Players</a> |
+  <a href="today.html">Today</a> |
+  <a href="fixtures.html">Fixtures</a> |
+  <a href="results.html">Results</a> |
+  <a href="standings.html">Standings</a> |
+  <a href="login.html">Login</a>
+  <hr>
+</div>
+`);
